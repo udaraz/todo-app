87f57c9f96e512490b4a427c912d1e385b1821e9
udara herath <usbherath@gmail.com>
2025-12-11 21:17:36 +0530

    Fix routing issue in client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import TodoList from "./components/Todo/TodoList.tsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 import ProtectedRoute from "./utility/ProtectedRoute.tsx";
 import Login from "./components/Auth/Login.tsx";
 import './index.css';
 
 const router = createBrowserRouter([
-
+    {
+        path: "/",
+        element: <Navigate to="/todos" replace />
+    },
     { path: "/login", element: <Login /> },
     {
         element: <ProtectedRoute/>,
